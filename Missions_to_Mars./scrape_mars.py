32210e505305d62f82e02ea2cdaa845ea176401d
Karla-Flores <ms.kaflores@gmail.com>
2021-07-16 06:34:26 -0500

    Set Executable Path & Initialize Chrome Browser

--- Missions_to_Mars./scrape_mars.py
+++ Missions_to_Mars./scrape_mars.py
@@ -6,3 +6,9 @@ from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 from pprint import pprint as pp
 
+# Set Executable Path & Initialize Chrome Browser
+def init_browser():
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+# NASA Mars News Site Web Scraper
