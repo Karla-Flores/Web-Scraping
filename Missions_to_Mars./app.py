36b5fd603937deb6a213b12b88d02885e1de8e43
Karla-Flores <ms.kaflores@gmail.com>
2021-07-16 12:45:47 -0500

    Starting scraping for first title and paragraph

--- Missions_to_Mars./app.py
+++ Missions_to_Mars./app.py
@@ -10,22 +10,22 @@ from config import k_mongo
 app = Flask(__name__)
 
 # Using flask_pymongo to set up mongo coneection
-app.config["MONGO_URI"] = "k_mongo"
+app.config['MONGO_URI'] = 'k_mongo'
 mongo = PyMongo(app)
 
 # Home
-@app.route("/")
+@app.route('/')
 def index():
     info = mongo.db.info.find_one()
-    return render_template("index.html", info=info)
+    return render_template('index.html', info=info)
 
 #
-@app.route("/scrape")
+@app.route('/scrape')
 def scraper():
     info = mongo.db.info
     info_data = scrape_mars.scrape()
     info.update({}, info_data, upsert=True)
-    return redirect("/", code=302)
+    return redirect('/', code=302)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- Missions_to_Mars./scrape_mars.py
+++ Missions_to_Mars./scrape_mars.py
@@ -12,3 +12,25 @@ def init_browser():
     return Browser("chrome", **executable_path, headless=False)
 
 # NASA Mars News Site Web Scraper
+def mars_news(browser):
+    # Visit the NASA Mars News Site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+
+    # Get First title and paragraph
+    browser.is_element_present_by_css("div.list_text", wait_time=0.5)
+    
+    html = browser.html
+    news_soup = Beautifulsoup(html, "html.parser")
+
+    try:
+        element = news_soup.select_one("div.list_text")
+        element.find("div", class_="content_title")
+        
+        title = element.find('div', class_='content_title').get_text()
+        paragraph = element.find('div', class_='article_teaser_body').get_text()
+    
+    except AttributeError:
+        return None, None
+
+    return print(f'Title: {title}/n'), print(f'Paragraph: {paragraph}')
